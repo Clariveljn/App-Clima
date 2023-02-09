cd300fc9f491b02dd419a85b7615050d20625f3c
Clariveljn <cjeldres5@gmail.com>
2023-02-09 17:16:54 -0300

    Consultando clima a la API

--- src/context/ClimaProvider.jsx
+++ src/context/ClimaProvider.jsx
@@ -1,13 +1,17 @@
 import { useState, createContext } from "react";
+import axios from "axios";
 
 const ClimaContext = createContext()
 
 const ClimaProvider = ({children}) => {
+    
 
     const [busqueda, setBusqueda] = useState({
         pais: '',
         ciudad: ''
     })
+
+    const [resultado, setResultado] = useState({});
     
     const datosBusqueda = e => {
         setBusqueda({
@@ -16,15 +20,35 @@ const ClimaProvider = ({children}) => {
         })
     }
 
-    const consultarClima = datos => {
-        console.log(datos)
+    const consultarClima = async datos => {
+        try {
+            const {pais, ciudad } = datos
+
+            const appId = import.meta.env.VITE_API_KEY
+
+            const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${appId}`
+
+            const { data } = await axios(url)
+            const {lat, lon } = data[0]
+            
+            const urlClima = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
+
+            const { data: clima } = await axios(urlClima)
+            setResultado(clima)
+            
+        } catch (error) {
+            console.log(error)
+            
+        }
     }
+
     return (
         <ClimaContext.Provider
             value={{
                 busqueda,
                 datosBusqueda,
-                consultarClima
+                consultarClima,
+                resultado
             }}
         >
             {children}
