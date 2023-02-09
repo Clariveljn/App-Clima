7d869199a63b60d1d1033e133f01435ba2e8f991
Clariveljn <cjeldres5@gmail.com>
2023-02-09 12:49:20 -0300

    Agregue formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -2,7 +2,42 @@ import React from 'react'
 
 const Formulario = () => {
   return (
-    <div>Formulario</div>
+    <div className="contenedor">
+        <form>
+            <div className="campo">
+                <label htmlFor="pais">País</label>
+                <select 
+                    name="pais" 
+                    id="pais"
+                >
+                    <option value=""> Seleccione un país</option>
+                    <option value="CL">Chile</option>
+                    <option value="US">Estados Unidos</option>
+                    <option value="MX">México</option>
+                    <option value="AR">Argentina</option>
+                    <option value="CO">Colombia</option>
+                    <option value="CR">Costa Rica</option>
+                    <option value="ES">España</option>
+                    <option value="PE">Perú</option>
+                    <option value="BR">Brasil</option>   
+                </select>
+            </div>
+            <div className="campo">
+                <label htmlFor="ciudad">Ciudad</label>
+                <input 
+                    type="text"
+                    id="ciudad"
+                    name="ciudad"
+                />
+            </div>
+
+            <input 
+                type="submit"
+                value="Consultar Clima" 
+                />
+        </form>
+
+    </div>
   )
 }
 
